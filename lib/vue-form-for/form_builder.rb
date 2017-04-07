f3e46c3e8bf8ec81f98dc105ff6db4da6865ed73
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-04-07 09:32:56 +0900

    Allow the form builder to resolve :v option.

--- lib/vue-form-for/form_builder.rb
+++ lib/vue-form-for/form_builder.rb
@@ -3,10 +3,40 @@ module VueFormFor
     (field_helpers - [:label, :fields_for]).each do |selector|
       class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
         def #{selector}(method, options = {})
+          resolve_vue_options(options)
           options[:"v-model"] ||= "\#{@object_name}.\#{method}"
           super(method, options)
         end
       RUBY_EVAL
+    end
+
+    def label(method, text = nil, options = {}, &block)
+      resolve_vue_options(options)
+      super(method, text, options, &block)
+    end
+
+    def submit(value = nil, options = {})
+      resolve_vue_options(options)
+      super(value, options)
+    end
+
+    def button(value = nil, options = {}, &block)
+      resolve_vue_options(options)
+      super(value, options, &block)
+    end
+
+    private def resolve_vue_options(options)
+      if options[:v].kind_of?(Hash)
+        h = options.delete(:v)
+        h.each do |key, value|
+          case key
+          when /\A[:@]/
+            options[key] = value
+          else
+            options[:"v-#{key}"] = value
+          end
+        end
+      end
     end
   end
 end
