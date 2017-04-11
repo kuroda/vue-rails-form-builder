18810b0cfb9fd85370c2bbefb5a0986bb410f9f5
Tsutomu Kuroda <t-kuroda@oiax.jp>
2017-04-11 18:07:55 +0900

    Fix check_box and radio_button helper methods
    These methods have difference signature.

--- lib/vue-form-for/form_builder.rb
+++ lib/vue-form-for/form_builder.rb
@@ -4,7 +4,8 @@ module VueFormFor
   class FormBuilder < ActionView::Helpers::FormBuilder
     include VueFormFor::VueOptionsResolver
 
-    (field_helpers - [:label, :fields_for]).each do |selector|
+    (field_helpers - [:label, :check_box, :radio_button, :fields_for])
+      .each do |selector|
       class_eval <<-RUBY_EVAL, __FILE__, __LINE__ + 1
         def #{selector}(method, options = {})
           resolve_vue_options(options)
@@ -17,6 +18,16 @@ module VueFormFor
     def label(method, text = nil, options = {}, &block)
       resolve_vue_options(options)
       super(method, text, options, &block)
+    end
+
+    def check_box(method, options = {}, checked_value = "1", unchecked_value = "0")
+      resolve_vue_options(options)
+      super(method, options, checked_value, unchecked_value)
+    end
+
+    def radio_button(method, tag_value, options = {})
+      resolve_vue_options(options)
+      super(method, tag_value, options)
     end
 
     def submit(value = nil, options = {})
